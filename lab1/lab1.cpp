75d789163c87834ff820d13b87b8f6a8c2570fb4
Alex8608 <aleshka.vlad@gmail.com>
2023-10-18 00:32:48 +0400

    Make set (unique)

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#include "lab1.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int iarray1[] = { 1, 3, 2, 5, 6, 6 ,6,2,1 };//5
+	int iarray2[] = { 4, 5, 7, 8, 9 };//5
+
+	float farray1[] = { 1.12, 2.33, 1.45, 5.76, 2.33, 2.33, 9.875 };//7
+	float farray2[] = { 1.34, 3.23, 1.45, 5.76, 8.34 };//5
+
+	auto iset1 = MySet<int>(iarray1, 9);
+	auto iset2 = MySet<int>(iarray2, 5);
+
+	auto fset1 = MySet<float>(farray1, 7);
+	auto fset2 = MySet<float>(farray2, 5);
+}
--- lab1/lab1.h
+++ lab1/lab1.h
@@ -1,1 +1,73 @@
-﻿
+﻿#pragma once
+#include <random>
+
+template<typename T>
+class MySet {
+private:
+	//static const double kEpsilon = 0.001;
+
+	T* data_;
+
+	int size_;
+
+	int ElementIsHere(T* data, int size, const T& value) {
+		for (int i = 0; i < size; ++i)
+		{
+			if (data[i] == value)
+				return i;
+		}
+		return -1;
+	}
+
+	T* GetUniqueData(T* data, int size, int* newsize) {
+		T* uniquedata = new T[size]();
+		int i = 0;
+		int j = 0;
+		while (i < size && j < size) {
+			if (ElementIsHere(uniquedata, *newsize, data[j]) == -1) {
+				uniquedata[i] = data[j];
+				*newsize += 1;
+				++i;
+				++j;
+			}
+			else ++j;
+		}
+		return uniquedata;
+	}
+
+	void Sort() {
+		for (int i = 0; i < size_; ++i)
+		{
+			for (int j = 0; j < size_ - 1; j++)
+			{
+				if (data_[j] > data_[j + 1]) {
+					T temp = data_[j];
+					data_[j] = data_[j + 1];
+					data_[j + 1] = temp;
+				}
+			}
+		}
+	}
+public:
+	MySet() :data_(nullptr), size_(0) {}
+
+	MySet(T* data, int size) {
+		int* newsize = new int(0);
+
+		data_ = GetUniqueData(data, size, newsize);
+
+		size_ = *newsize;
+
+		this->Sort();
+
+		delete newsize;
+	}
+
+	int getsize() const {
+		return size_;
+	}
+
+	const T* getdata() const {
+		return data_;
+	}
+};

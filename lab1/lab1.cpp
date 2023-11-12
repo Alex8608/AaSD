e472b604f4c0d86d40bb17ac9a1a6786f3c6935f
Alex8608 <aleshka.vlad@gmail.com>
2023-11-12 21:27:55 +0400

    make full set and test this

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -5,19 +5,67 @@ using namespace myset;
 using namespace std;
 
 int main() {
-	int iarray1[] = { 1, 3, 2, 5, 6, 6 ,6,2,1 };//5
+	int iarray1[] = { 1, 3, 2, 5, 6, 6, 6, 2, 1 };//5
 	int iarray2[] = { 4, 5, 7, 8, 9 };//5
-
-	float farray1[] = { 1.12, 2.33, 1.45, 5.76, 2.33, 2.33, 9.875 };//7
-	float farray2[] = { 1.34, 3.23, 1.45, 5.76, 8.34 };//5
+	int iarray3[] = { 1, 2, 3, 4, 5, 3, 2 };//5
+	int iarray4[] = { 1, 2, 3, 4, 5, 6, 6, 1 };//6
+	float farray1[] = { 1.12, 2.3, 1.45, 7.26, 2.33, 2.33, 8.875 };//6
+	float farray2[] = { 1.34, 3.23, 1.45, 7.26, 8.34 };//5
+	string sarray[] = { "alex", "lab", "on", "laptop" };//4
+	pair<int, double> parray1[] = { make_pair(1, 1.12), make_pair(4, 3.14), make_pair(3, 7.12), make_pair(7, 4.12), make_pair(4, 3.14) };//5
+	pair<int, double> parray2[] = { make_pair(1, 1.1), make_pair(4, 3.14), make_pair(1, 1.1), make_pair(5, 7.12), make_pair(6, 4.88), make_pair(7, 4.12) };//5
 
 	auto iset1 = MySet<int>(iarray1, 9);
 	auto iset2 = MySet<int>(iarray2, 5);
 
-	cout << iset1 << endl << iset2 << endl;
+	auto iset3 = MySet<int>(iarray3, 5);
+	auto iset4 = MySet<int>(iarray4, 6);
 
 	auto fset1 = MySet<float>(farray1, 7);
 	auto fset2 = MySet<float>(farray2, 5);
 
-	cout << fset1 << endl << fset2 << endl;
+	auto sset1 = MySet<string>(sarray, 4);
+
+	auto pset1 = MySet<pair<int, double>>(parray1, 5);
+	auto pset2 = MySet<pair<int, double>>(parray2, 5);
+
+	cout << "Set of int" << "\n";
+	cout << iset1 << "\n\n";
+	cout << "Set of float" << "\n";
+	cout << fset1 << "\n\n";
+	cout << "Set of string" << "\n";
+	cout << sset1 << "\n\n";
+	cout << "Set of pair" << "\n";
+	cout << pset1 << "\n\n";
+
+	cout << "Sum of sets(int):" << "\n";
+	cout << iset1 << " + " << iset2 << " = ";
+	auto sum = iset1 + iset2;
+	cout << sum;
+	cout << "\n\n";
+
+	cout << "Difference of sets(float):" << "\n";
+	cout << fset1 << "- " << fset2 << " = ";
+	auto dif = fset1 - fset2;
+	cout << dif;
+	cout << "\n\n";
+
+	cout << "+= and -= with string set:\n";
+	cout << sset1 << "\n";
+	sset1 += "samara";
+	sset1 -= "lab";
+	cout << sset1;
+	cout << "\n\n";
+
+	cout << "Difference of sets(pair):" << "\n";
+	cout << pset1 << "- " << pset2 << " = ";
+	auto dif_pair = pset1 - pset2;
+	cout << dif_pair;
+	cout << "\n\n";
+
+	cout << "Intersection of sets(int):" << "\n";
+	cout << iset1 << "and " << iset2 << " = ";
+	auto inter_int = iset1.Intersection(iset2);
+	cout << inter_int;
+	cout << "\n\n";
 }
--- lab1/lab1.h
+++ lab1/lab1.h
@@ -64,12 +64,81 @@ namespace myset {
 			delete newsize;
 		}
 
+		MySet(int size, const T begin, const T end) : size_(size), data_(new T[size]) {
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_real_distribution<> dis(begin, end);
+			for (int i = 0; i < size; ++i) {
+				auto value = 0.0;
+				int index = 0;
+				while (index != -1) {
+					value = dis(gen);
+					index = ElementIsHere(data_, size_, value);
+				}
+				data_[i] = value;
+			}
+			this->Sort();
+		}
+
+		MySet(const MySet& rhs) : size_(rhs.GetSize()), data_(new T[rhs.GetSize()]) {
+			for (int i = 0; i < rhs.GetSize(); i++)
+			{
+				data_[i] = rhs[i];
+			}
+		}
+
+		MySet& operator=(const MySet& rhs) {
+			MySet copy(rhs);
+			swap(copy);
+			return *this;
+		}
+
+		bool operator==(const MySet& rhs) {
+			if (size_ != rhs.GetSize()) return false;
+			for (int i = 0; i < size_; i++)
+			{
+				if (std::abs(data_[i] - rhs[i]) >= kEpsilon) return false;
+			}
+			return true;
+		}
+
+		bool operator!=(const MySet& rhs) {
+			return !(*this == rhs);
+		}
+
+		int Find(const T& value) const {
+			for (int i = 0; i < size_; i++)
+			{
+				if (data_[i] == value) return i;
+			}
+			return -1;
+		}
+
+		MySet<T> Intersection(const MySet<T>& rhs) {
+			auto dif = *this - rhs;
+
+			return (*this - dif);
+		}
+
 		int GetSize() const {
 			return size_;
 		}
 
-		const T* getdata() const {
+		int& GetSize() {
+			return size_;
+		}
+
+		const T* GetData() const {
 			return data_;
+		}
+
+		T*& GetData() {
+			return data_;
+		}
+
+		void Swap(MySet<T>& rhs) noexcept {
+			std::swap(data_, rhs.GetData());
+			std::swap(size_, rhs.GetSize());
 		}
 
 		T& operator[](int index) {
@@ -86,18 +155,154 @@ namespace myset {
 			return data_[index];
 		}
 
+		MySet& operator+=(const MySet& rhs) {
+			int new_size = size_ + rhs.GetSize();
+
+			T* new_data = new T[new_size]();
+
+			for (int i = 0; i < size_; ++i)
+			{
+				new_data[i] = data_[i];
+			}
+
+			int j = 0;
+
+			for (int i = size_; i < new_size; ++i)
+			{
+				new_data[i] = rhs[j];
+				++j;
+			}
+
+			delete[] data_;
+
+			int* actual_size = new int(0);
+
+			data_ = GetUniqueData(new_data, new_size, actual_size);
+
+			delete[] new_data;
+
+			size_ = *actual_size;
+
+			this->Sort();
+
+			return *this;
+		}
+
+		MySet& operator-=(const MySet& rhs) {
+			for (int i = 0; i < rhs.GetSize(); i++)
+			{
+				int index = ElementIsHere(data_, size_, rhs[i]);
+				if (index != -1) {
+
+					this->Remove(index);
+				}
+			}
+			return *this;
+		}
+
+		MySet& operator+=(const T& value) {
+			if (ElementIsHere(data_, size_, value) != -1) {
+				return *this;
+			}
+			int new_size = size_ + 1;
+			T* new_data = new T[new_size];
+			for (int i = 0; i < size_; i++)
+			{
+				new_data[i] = data_[i];
+			}
+			new_data[new_size - 1] = value;
+			delete[] data_;
+			data_ = new_data;
+			size_ = new_size;
+			this->Sort();
+			return *this;
+		}
+
+		MySet& operator-=(const T& value) {
+			int index = ElementIsHere(data_, size_, value);
+			if (index == -1) {
+				return *this;
+			}
+			this->Remove(index);
+			return *this;
+		}
+
+		void Remove(int index) {
+			if (index < 0 || index >= size_) {
+				throw std::out_of_range("MySet::remove, index is out of range");
+			}
+			for (int i = index; i < size_ - 1; i++)
+			{
+				T temp = data_[i];
+				data_[i] = data_[i + 1];
+				data_[i + 1] = temp;
+			}
+			size_--;
+		}
+
+		template<typename R>
+		MySet operator+(const R& rhs) {
+			MySet copy(*this);
+			return copy += rhs;
+		}
+
+		template<typename R>
+		MySet operator-(const R& rhs) {
+			MySet copy(*this);
+			return copy -= rhs;
+		}
+
 		~MySet() {
 			delete[] data_;
 			size_ = 0;
 		}
 	};
 
+	template <>
+	MySet<std::string>::MySet(int size, std::string begin, std::string end) : size_(size), data_(new std::string[size])
+	{
+		int asc_start = begin[0];
+		int asc_end = end[0];
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(asc_start, asc_end);
+		for (int i = 0; i < size_; i++)
+		{
+			char* word = new char[6]();
+			for (int j = 0; j < 5; j++)
+			{
+				word[j] = dis(gen);
+			}
+			word[6] = '\n';
+			data_[i] = std::string(word);
+		}
+	}
+
+	template <>
+	MySet<std::pair<int, double>>::MySet(int size, std::pair<int, double> begin, std::pair<int, double> end) : size_(size), data_(new std::pair<int, double>[size])
+	{
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution<> dis_first(begin.first, end.first);
+		std::uniform_real_distribution<> dis_second(begin.second, end.second);
+		for (int i = 0; i < size; i++)
+		{
+			data_[i] = std::make_pair(dis_first(gen), dis_second(gen));
+		}
+	}
+
+	template<typename T, typename R>
+	std::ostream& operator<<(std::ostream& out, std::pair<T, R>& pair) {
+		out << "(" << pair.first << ", " << pair.second << ")";
+		return out;
+	}
+
 	template<typename T>
 	std::ostream& operator<<(std::ostream& out, MySet<T>& set) {
-		for (int i = 0; i < set.GetSize(); ++i)
+		for (size_t i = 0; i < set.GetSize(); i++)
 		{
-			std::cout << set[i] << " ";
-			if (i == 9) std::cout << "\n";
+			out << set[i] << " ";
+			if (i == 9) out << "\n";
 		}
 		return out;
 	}
